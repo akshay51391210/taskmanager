377142c844eb5b1cb7afe9e0f4d206754b6e7339
akshayy <somisettyakshay2@gmail.com>
2025-08-17 17:38:09 +1000

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import Projects from './pages/Projects';
+import Bids from './pages/Bids';
+
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/bids" element={<Bids />} />
       </Routes>
     </Router>
   );
